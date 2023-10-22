478613d32d8c3f247303ef2fe4d34b937a8318b6
Tim <tim.spiele1@freenet.de>
2023-10-22 18:09:17 +0200

    delete toolbar component and add new components
    Added new components:
    -user
    -dashboard
    
    added routing for both

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
+    DashboardComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
