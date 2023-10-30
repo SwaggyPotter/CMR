efa68b6efe7521c5c53248fa4d35e4ea5e70e93f
Tim <tim.spiele1@freenet.de>
2023-10-30 17:41:22 +0100

    get user based on id in url

--- src/app/detail-card/detail-card.component.ts
+++ src/app/detail-card/detail-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail-card',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./detail-card.component.scss']
 })
 export class DetailCardComponent {
+  bankName : any
+  constructor(private route: ActivatedRoute) {
 
+  }
+
+  ngOnInit(){
+      this.bankName = this.route.snapshot.paramMap.get('id')
+      console.log('the id is: ',this.bankName)
+  }
 }
