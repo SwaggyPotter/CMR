b70d6bdc7cf3d72377b6cc9b59aeacf9bceb3a3e
Tim <tim.spiele1@freenet.de>
2023-10-31 20:02:24 +0100

    update address in firebase

--- src/app/edit-user-adress-dialog/edit-user-adress-dialog.component.ts
+++ src/app/edit-user-adress-dialog/edit-user-adress-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+import { doc, getFirestore, setDoc } from 'firebase/firestore';
 import { User } from '../models/user.class';
 
 @Component({
@@ -8,13 +12,33 @@ import { User } from '../models/user.class';
 })
 export class EditUserAdressDialogComponent {
   user: any = User;
-  loading : boolean = false;
-  
+  loading: boolean = false;
+  db: any
+  firestore: Firestore = inject(Firestore)
+
+
   constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDxJcs5hA7ww_7W2MWnRmGbs13n5sn1_fA",
+      authDomain: "simple-crm-system-9f5e8.firebaseapp.com",
+      projectId: "simple-crm-system-9f5e8",
+      storageBucket: "simple-crm-system-9f5e8.appspot.com",
+      messagingSenderId: "988410038077",
+      appId: "1:988410038077:web:ae12fc4879f67f2ceba754",
+      measurementId: "G-J861YGKZ2C"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    this.db = getFirestore(app);
 
   }
 
-  saveUser(){
-
+  async updateUser() {
+    const db = getDatabase();
+    this.loading = true;
+    let userAsJson = this.user.toJSON()
+    //await addDoc(collection(this.firestore, 'users'), { userAsJson });
+    await setDoc(doc(this.firestore, "users", this.user.id), userAsJson);
+    this.loading = false;
   }
 }
