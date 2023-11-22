fff8acfbf3230e27ea3207cef46bffd4cfa32d5f
Tim <89969636+SwaggyPotter@users.noreply.github.com>
2023-11-22 03:08:03 +0100

    Get Login data of all user

--- src/app/sing-in/sing-in.component.ts
+++ src/app/sing-in/sing-in.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { initializeApp } from '@angular/fire/app';
+import { Firestore } from '@angular/fire/firestore';
 import { FormControl, FormGroupDirective, NgForm, Validators, FormsModule, ReactiveFormsModule, } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
 
 @Component({
   selector: 'app-sing-in',
@@ -11,9 +14,50 @@ import { ErrorStateMatcher } from '@angular/material/core';
 
 
 export class SingInComponent {
+  userId: any;
+  firestore: Firestore = inject(Firestore);
+  user: any = {};
+  db: any;
+  users: any;
+  allUsers: any;
+  title: string = '';
+  note: string = '';
+  itemNumber: number = 0;
+  loading: boolean = false;
+  userAmount: number = 0;
+  userArray: any = [];
+  isDataReady: boolean = false;
+  incomeArray: any = [];
+  openDocDataIncome: any;
+  sumTotal: number = 0;
+
+
+
   hide: boolean = true;
 
   myFunction() {
     this.hide = !this.hide;
+  }
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDxJcs5hA7ww_7W2MWnRmGbs13n5sn1_fA",
+      authDomain: "simple-crm-system-9f5e8.firebaseapp.com",
+      projectId: "simple-crm-system-9f5e8",
+      storageBucket: "simple-crm-system-9f5e8.appspot.com",
+      messagingSenderId: "988410038077",
+      appId: "1:988410038077:web:ae12fc4879f67f2ceba754",
+      measurementId: "G-J861YGKZ2C"
+    };
+    const app = initializeApp(firebaseConfig);
+    this.db = getFirestore(app);
+    this.loadLoginnData()
+  }
+
+  async loadLoginnData() {
+    const querySnapshot = await getDocs(collection(this.db, "logins"));
+    querySnapshot.forEach((doc) => {
+      console.log(JSON.parse(doc.data()['emailsAndPasswords'][0]) )
+    });
   }
 }
