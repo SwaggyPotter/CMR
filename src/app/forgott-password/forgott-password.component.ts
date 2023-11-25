552f4a3a62afe20aa8ffcea28474acfd81d2d9d2
Tim <tim.spiele1@freenet.de>
2023-11-25 14:55:51 +0100

    forgott password pages createt

--- src/app/forgott-password/forgott-password.component.ts
+++ src/app/forgott-password/forgott-password.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { initializeApp } from '@angular/fire/app';
+import { Firestore } from '@angular/fire/firestore';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormsModule, ReactiveFormsModule, } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { EmailAuthCredential } from 'firebase/auth';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-forgott-password',
@@ -6,5 +13,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgott-password.component.scss']
 })
 export class ForgottPasswordComponent {
+  userId: any;
+  firestore: Firestore = inject(Firestore);
+  user: any = {};
+  db: any;
+  users: any;
+  allUsers: any;
+  title: string = '';
+  note: string = '';
+  itemNumber: number = 0;
+  loading: boolean = false;
+  userAmount: number = 0;
+  userArray: any = [];
+  isDataReady: boolean = false;
+  incomeArray: any = [];
+  openDocDataIncome: any;
+  sumTotal: number = 0;
+  email: string = '';
+  password: string = '';
+  hide: boolean = true;
+  passwordEmailWrong: boolean = false;
+  emailEnter: boolean = true;
+  codeEnter: boolean = false;
+  enterNEwPw: boolean = false;
+
+  constructor(private _router: Router, private _activatedRoute: ActivatedRoute) {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDxJcs5hA7ww_7W2MWnRmGbs13n5sn1_fA",
+      authDomain: "simple-crm-system-9f5e8.firebaseapp.com",
+      projectId: "simple-crm-system-9f5e8",
+      storageBucket: "simple-crm-system-9f5e8.appspot.com",
+      messagingSenderId: "988410038077",
+      appId: "1:988410038077:web:ae12fc4879f67f2ceba754",
+      measurementId: "G-J861YGKZ2C"
+    };
+    const app = initializeApp(firebaseConfig);
+    this.db = getFirestore(app);
+  }
+
+
+
+
+
+
+
 
 }
